b5cff5618fc574268795745443555807eae99c50
Benedek Vass <vabe44@gmail.com>
2017-11-14 03:33:26 +0100

    table name fix

--- app/models/category.js
+++ app/models/category.js
@@ -13,7 +13,7 @@ module.exports = function (sequelize, DataTypes) {
             allowNull: true
         },
     }, {
-            tableName: 'shoutout_categories',
+            tableName: 'categories',
             underscored: true
         });
 };
